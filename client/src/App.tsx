d1d45f575e4c7475ae1fadf5876bb7d4afcb4142
yasaidaisuki <peng.dingkai@gmail.com>
2023-09-09 15:49:23 -0400

    client & server setup

---
+++ client/src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Login from './pages/Login'
+
+function App() {
+
+  return (
+    <>
+      <Login/>
+    </>
+  )
+}
+
+export default App
---
+++ client/src/pages/Login.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import LoginForm from '../components/login/LoginForm'
+
+const Login = () => {
+  return (
+    <>
+        <LoginForm/>
+    </>
+  )
+}
+
+export default Login
