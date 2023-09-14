667182880c478700ed13a71fdb6034bf27e0ff8f
yasaidaisuki <peng.dingkai@gmail.com>
2023-09-14 00:56:59 -0400

    added mongodb, express, cors, dotenv, server.js, index.js | connect to MongoClient

--- server/index.js
+++ server/index.js
@@ -1,11 +1,31 @@
-const express = require('express')
-const app = express()
-const port = 3000
+import app from "./server.js"
+import mongodb from "mongodb"
+import dotenv from "dotenv"
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+dotenv.config()
+const MongoClient = mongodb.MongoClient;
 
-app.listen(port, () => {
-  console.log(`Server started on localhost:${port}`)
-})
+// if doesnt get process.env.PORT then 3000
+const port = process.env.PORT || 3000;
+
+// connecting to db
+MongoClient.connect(
+  process.env.RESTREVIEWS_DB_URI, 
+  {
+    // how many ppl can access
+    poolSize: 50,
+    // how long wait until exit
+    waitQueueTimeoutMS: 2500,
+    useNewUrlParse: true
+  }
+  // catch error
+  .catch(err => {
+    console.error(err.stack);
+    process.exit;
+  })
+  .then(async client => {
+    app.listen(port, () => {
+      console.log('listening on port ${port}');
+    })
+  })
+)
