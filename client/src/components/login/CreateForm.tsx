2734e14f040657eff3a3ee8bdbf6003c00c1aa3e
yasaidaisuki <peng.dingkai@gmail.com>
2023-12-24 22:05:31 -0500

    add routes, update mongoose, dotenv, cors, connected db, added user schemas for login

--- client/src/components/login/CreateForm.tsx
+++ client/src/components/login/CreateForm.tsx
@@ -1,15 +1,40 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useState } from 'react';
+import axios from 'axios'
 
 function CreateForm() {
+  const history=useNavigate();
 
   const [email, setEmail] = useState("");
-  const [pass, setPass] = useState("");
+  const [password, setPass] = useState("");
 
-  const handleSubmit = (e) => {
+  async function submit(e) {
     e.preventDefault();
     
+    try{
+
+      await axios.post("http://localhost:5173/create",{
+        email,password
+      })
+      .then(res => {
+        if (res.data == "exist"){
+          alert("User already exists")
+        }
+        else if (res.data == "nonexist"){
+          history("/home,",{state:{id:email}})
+        }
+      })
+      .catch(e=>{
+        alert("incorrect login");
+        console.log(e)
+      })
+
+    }
+    catch(e) {
+      console.log(e);
+    }
+
   }
 
   return (
@@ -21,16 +46,15 @@ function CreateForm() {
               <h2 className="text-4xl text-black text-center">CREATE ACCOUNT</h2>
             </div>
           </div>
-          <form onSubmit={handleSubmit}>
+          <form action="POST">
             <div className="mt-4 space-y-6">
               <div className="col-span-full">
                 <label className="block mb-3 text-sm font-medium text-gray-600" htmlFor="email">
                   Email
                 </label>
                 <input
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
                   type="email"
+                  onChange={(e) =>  {setEmail(e.target.value)}}
                   className="block w-full px-6 py-3 text-black bg-white border border-gray-200 rounded-full appearance-none placeholder:text-gray-400 focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm"
                   placeholder="user@email.com"
                   autoComplete="off"
@@ -41,9 +65,8 @@ function CreateForm() {
                   Password
                 </label>
                 <input
-                  value={pass}
-                  onChange={(e) => setPass(e.target.value)}
                   type="password"
+                  onChange={(e) => {setPass(e.target.value)}}
                   className="block w-full px-6 py-3 text-black bg-white border border-gray-200 rounded-full appearance-none placeholder:text-gray-400 focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm"
                   placeholder="******"
                   autoComplete="off"
@@ -54,6 +77,7 @@ function CreateForm() {
                 <button
                   className="items-center justify-center w-full px-6 py-2.5 text-center text-white duration-200 bg-black border-2 border-black rounded-full nline-flex hover:bg-transparent hover:border-black hover:text-black focus:outline-none focus-visible:outline-black text-sm focus-visible:ring-black"
                   type="submit"
+                  onClick={submit}
                 >
                   Create New Account
                 </button>
