e3c2775a1e79ba05182ddbc349e30ac7c1df7f9a
samippp <Sami.peng@hotmail.com>
2024-03-18 18:06:05 -0400

    links should probably work

--- client/src/components/Home/Body.tsx
+++ client/src/components/Home/Body.tsx
@@ -8,11 +8,15 @@ export default function Body() {
   const [gender, setGender] = useState("")
   const [dataSet, setData] = useState([])
 
-  async function viewItem(brand, name, id) {
-    window.open(`https://www.ssense.com/en-ca/product/${brand}/${name}/${id}`, '_blank');
+  async function viewItem(brand, name, id, seller) {
+    if(seller == "ss")
+      window.open(`https://www.ssense.com/en-ca/${gender}/product/${brand}/${name}/${id}`, '_blank');
+    else
+      window.open(`https://www.farfetch.com/ca/shopping/${gender}/${brand} ${name} ${id}.aspx`)
   }
 
   async function getMaleItems() {
+    setGender("men")
     await axios.get('http://localhost:5000/getMaleItems')
       .then((response) => {
         const itemData = (response.data).rows
@@ -26,6 +30,7 @@ export default function Body() {
   }
 
   async function getFemaleItems() {
+    setGender("women")
     await axios.get('http://localhost:5000/getFemaleItems')
       .then((response) => {
         const itemData = (response.data).rows
@@ -70,7 +75,7 @@ export default function Body() {
                   <span>${itemData.price}</span>
                   <span>{itemData.brand}</span>
                   <br />
-                  <button type="button" onClick={() => viewItem(itemData.brand, itemData.name, itemData.id)} className="text-white bg-gray-800 hover:bg-gray-900 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
+                  <button type="button" onClick={() => viewItem(itemData.brand, itemData.name, itemData.id, itemData.seller)} className="text-white bg-gray-800 hover:bg-gray-900 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
                     View
                   </button>
                   <button type="button" className="text-white bg-gray-800 hover:bg-gray-900  font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
